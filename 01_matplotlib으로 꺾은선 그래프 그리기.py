49b36bae9539f1ab7e9abdd7fa514659beae7c61
Kyunghee Lee <leekh090163@gmail.com>
2020-07-14 22:55:03 +0900

    new file

---
+++ 01_matplotlib으로 꺾은선 그래프 그리기.py
@@ -0,0 +1,12 @@
+# 시각화의 기본인 matplotlib 라이브러리를 사용해 그래프를 그려보자.
+
+# 라이브러리 불러오기
+import matplotlib.pyplot as plt
+
+# 데이터 시각화하기(꺾은선그래프)
+plt.title('그래프 제목')
+plt.xlabel('x축 제목')
+plt.ylabel('y축 제목')
+plt.plot(데이터프레임 column1, 데이터프레임 column2) #(x축, y축)
+
+plt.show()
