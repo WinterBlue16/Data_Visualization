48f753c84b92d14eff964bd6b954c53a3694f867
Kyunghee Lee <leekh090163@gmail.com>
2020-07-31 18:59:11 +0900

    dataframe 추가

--- 01_matplotlib으로 꺾은선 그래프 그리기.py
+++ 01_matplotlib으로 꺾은선 그래프 그리기.py
@@ -2,11 +2,15 @@
 
 # 라이브러리 불러오기
 import matplotlib.pyplot as plt
+import pandas as pd
+
+# 데이터프레임 만들기
+df = pd.DataFrame(data={'라면 종류': ['신라면', '삼양라면', '불닭볶음면', '너구리', '열라면'], '선호도' : [5, 2, 1, 3, 3]})
 
 # 데이터 시각화하기(꺾은선그래프)
-plt.title('그래프 제목')
-plt.xlabel('x축 제목')
-plt.ylabel('y축 제목')
-plt.plot(데이터프레임 column1, 데이터프레임 column2) #(x축, y축)
+plt.title('나의 라면 브랜드 선호도')
+plt.xlabel('라면 종류')
+plt.ylabel('선호도')
+plt.plot(df['라면 종류'], df['선호도']) #(x축, y축)
 
 plt.show()
